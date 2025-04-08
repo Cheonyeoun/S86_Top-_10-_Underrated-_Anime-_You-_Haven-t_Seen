443e3ffc1be1976aec269fe0177005bbcc741980
Cheonyeoun <vigneshpandiyan08@gmail.com>
2025-04-08 11:34:09 +0530

    basic codebase

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/pages/NavBar';
+
+import Home from './components/pages/Home';
+
+
+function App() {
+
+  return (
+    <>
+
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path ="/" element={<Home/>} />
+      </Routes>
+    </Router>
+    
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/pages/Home.jsx
@@ -0,0 +1,23 @@
+function Home(){
+
+
+    return(
+    
+    <>
+    
+    <div>
+
+        <h1>Hey there, Welcome to the site </h1>
+
+
+
+
+    </div>
+    
+    
+    </>
+
+    )
+}
+
+export default Home;
