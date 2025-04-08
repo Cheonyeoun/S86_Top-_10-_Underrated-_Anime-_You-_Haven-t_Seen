ced7a9eb034b750fe43e121ccc279c69b51ca330
Cheonyeoun <vigneshpandiyan08@gmail.com>
2025-04-08 23:35:53 +0530

    Add basic express server with / and /ping routes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+const PORT = 3000;
+
+app.get('/',(req,res) =>{
+    return res.send("Hey There Welcome To The Server!")
+});
+app.get('/ping',(req,res)=>{
+    return res.send("pong");
+});
+
+app.listen(PORT,()=>{
+
+    console.log(`Server is Running on http://localhost:${PORT}`);
+});
